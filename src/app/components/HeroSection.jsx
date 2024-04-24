bb123a42f111ae1d2608de4fb8531a669c2a1d76
ijaz ahmad <ijazbangash>
2024-04-24 12:37:37 +0500

    initial commit

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+        <div className='col-span-7 place-self-center'>
+        <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
+        Hello, I'm ijaz
+        </h1>
+        <p className='text-[#ADB7BE] text-lg lg:text-xl '>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit 
+        </p>
+        </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
