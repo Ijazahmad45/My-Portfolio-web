7b5379ecef060aca8ddf2589fab7b3fb5c3bcb87
ijaz ahmad <ijazbangash>
2024-04-25 16:09:16 +0500

    PW-1 HeroSection created

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,12 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+    
+    <div class="container mx-auto px-12 py-4">
     <HeroSection/>
+    </div>
+    
     </main>
   );
 }
