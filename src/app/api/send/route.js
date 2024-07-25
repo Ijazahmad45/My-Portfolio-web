bb977c808d5360a0668a87dd059f757aa6ae9f35
ijaz Ahmad <ijazbangashi.b@gmail.com>
2024-07-25 14:17:56 +0500

    Error solved

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,24 +1,25 @@
-import { render } from "@react-email/render";
-// import { EmailTemplate } from '../../../../emails/index.jsx';
-import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// import { render } from "@react-email/render";
+// // import { EmailTemplate } from '../../../../emails/index.jsx';
+// import { Resend } from 'resend';
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 
-export async function POST( request, Response) {
-  const { email, userFirstName} = await request.json();
+// export async function POST( request, Response) {
+//   const { email, userFirstName} = await request.json();
 
-  const { data, error } = await resend.emails.send({
-    from: "Acme <OnBoarding2@resend.dev>",
-    to: {email},
-    subject: 'Thank you for your message!',
-    html: EmailTemplate({ userFirstName }),
-  });
-if (error) {
-  return Response.json(error);
-}
+//   const { data, error } = await resend.emails.send({
+//     from: "Acme <OnBoarding2@resend.dev>",
+//     to: {email},
+//     subject: 'Thank you for your message!',
+//     html: EmailTemplate({ userFirstName }),
+//   });
+// if (error) {
+//   return Response.json(error);
+// }
 
-  return Response.json({message: 'Email sent successfully!'});
+//   return Response.json({message: 'Email sent successfully!'});
 
-}
+// }
